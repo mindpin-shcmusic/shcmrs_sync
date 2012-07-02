9d9c0d1ef87cf9f09b4c3f57167785a4fdb67a82
Kaid <kaid@kaid.me>
2012-07-02 19:27:04 +0800

    validate folders

--- app/models/media_folder.rb
+++ app/models/media_folder.rb
@@ -3,4 +3,15 @@ class MediaFolder < ActiveRecord::Base
 
   has_many   :media_folders
   belongs_to :media_folder
+
+  validate do |folder|
+    valid = case folder.media_folder_id
+            when 0
+              true
+            else
+              MediaFile.exists?(folder.media_folder_id)
+            end
+
+    errors.add(:base, 'olala') unless valid
+  end
 end
