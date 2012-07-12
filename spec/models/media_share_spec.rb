988ab9bb99cfa56e428b8b741658d12e1adfef73
ben7th <ben7th@sina.com>
2012-07-12 13:09:00 +0800

    add media_share spec

--- spec/models/media_share_spec.rb
+++ spec/models/media_share_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe MediaShare do
-  pending '不能共享资源给自己'
+  it '不能共享资源给自己，如果这样做，会校验失败' do
+    pending '还没做'
+  end
+
   pending '向他人共享资源后，他人可以取到'
+  pending '可以修改一个资源的共享者列表'
 end
