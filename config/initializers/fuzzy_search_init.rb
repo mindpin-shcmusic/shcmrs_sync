93ce3b8eb4d33691a50c5070637b4fd4efff9fb8
Kaid <kaid@kaid.me>
2012-07-17 17:04:51 +0800

    rescue the potential exception when redis not installed

--- config/initializers/fuzzy_search_init.rb
+++ config/initializers/fuzzy_search_init.rb
@@ -1,11 +1,17 @@
-redis = Redis.new
-redis.select(4)
-store = Redis::Namespace.new 'shc:redis_search', :redis => redis
+begin
+  redis = Redis.new
+  redis.select(4)
+  store = Redis::Namespace.new 'shc:redis_search', :redis => redis
 
-Redis::Search.configure do |config|
-  config.redis = store
-  config.complete_max_length = 32
-  config.pinyin_match = true
+  Redis::Search.configure do |config|
+    config.redis = store
+    config.complete_max_length = 32
+    config.pinyin_match = true
+  end
+
+  User.new
+
+rescue => e
+  puts '没有安装redis，redis相关功能没有办法使用'
+  puts e
 end
-
-User.new
